0248a7447be73ca39aa8b756463cbb343a119737
SiphokaziT <34504728+SiphokaziT@users.noreply.github.com>
2024-01-08 18:49:45 +0200

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,39 @@
+print("hello Welcome to my first Python game")
+name = input("what is your name?")
+age = int(input("what is your age?"))
+print('welcome',name)
+
+if age >= 18:
+    print("you are eligible to play")
+wants_to_play = input("Do you want to continue playing?").lower()
+if wants_to_play == "yes":
+    print("lets play")
+
+left_or_right = input("first choice.... left or right (left/right)")
+if left_or_right == "right":
+    ans = input("nice,you follow the path")
+
+    if ans == "around":
+    elif ans == "across":
+else:
+    print("you lost")
+else:
+print("you fell down and lost")
+else:
+    print("cya")
+
+
+
+else:
+    print("hi", name, "you are not eligible to play Goodbye ")
+
+
+
+
+
+
+
+    
+
+
+
